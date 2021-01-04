cfe2f7e2f375573b0cadf1b3045e4b9a2af4c521
Jady Hom <hyjady@gmail.com>
2021-01-04 12:26:33 -0500

    setup server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const AppRouter = require("./routes/AppRouter");
+const PORT = process.env.PORT || 3001;
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+// Initialize Middleware
+app.use(logger("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client", "build")));
+// Initialize Middleware
+
+app.disable("X-Powered-By");
+
+app.get("/", (req, res) => res.send({ msg: "Server Working" }));
+app.use("/api", AppRouter);
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+);
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
