892746cb2a9b3eaf93860263139fa33e4e023ff5
Jady Hom <hyjady@gmail.com>
2021-01-05 19:06:11 -0500

    fixed translate service

--- client/src/services/TranslateService.js
+++ client/src/services/TranslateService.js
@@ -9,45 +9,20 @@ export const __TranslateText = async (languageId) => {
   }
 };
 
+export const __RecordTranslations = async (inputId, outputId) => {
+  try {
+    const res = await ApiClient.post(
+      `/translate/record/${inputId}/${outputId}`
+    );
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const __GetLanguages = async () => {
   try {
     const res = await ApiClient.get("/translate/getlanguages");
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const __GetInputs = async () => {
-  try {
-    const res = await ApiClient.get("/translate/getinput_translations");
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const __GetOutputs = async () => {
-  try {
-    const res = await ApiClient.get("/translate/getoutput_translations");
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const __DeleteInputs = async (inputId) => {
-  try {
-    const res = await ApiClient.delete(`/translate/deleteinput/${inputId}`);
-    return res.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-export const __DeleteOutputs = async (outputId) => {
-  try {
-    const res = await ApiClient.delete(`/translate/deleteoutput/${outputId}`);
     return res.data;
   } catch (error) {
     throw error;
