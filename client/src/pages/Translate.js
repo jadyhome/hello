7b37a5ec508c7901d80e62310952e60c25a8d333
Jady Hom <hyjady@gmail.com>
2021-01-05 19:02:21 -0500

    setup translate page

--- client/src/pages/Translate.js
+++ client/src/pages/Translate.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Translate = (props) => {
+  const [text, setText] = useState("");
+
+  const handleChange = ({ target }) => {
+    setText(target.value);
+  };
+
   return (
     <div className="translate-page">
-      Translate
+      <div className="nav">
+        {<Link to="/">home</Link>}
+        {<Link to="/saved">saved</Link>}
+      </div>
+
+      <div className="input">
+        <textarea onChange={handleChange} />
+      </div>
+      <div className="output">
+        <textarea onChange={handleChange} />
+      </div>
     </div>
   );
 };
