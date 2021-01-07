cb5ad8335df4e55aa564d2e0afd498f964cc0e0f
Jady Hom <hyjady@gmail.com>
2021-01-06 19:52:05 -0500

    saved page: working functions and services

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { __GetInputs, __DeleteInputs } from "../services/InputService";
+import { __GetOutputs, __DeleteOutputs } from "../services/OutputService";
 
 const Saved = (props) => {
+  const [input, setInput] = useState([]);
+  const [output, setOutput] = useState([]);
+
+  useEffect(() => {
+    getInputs();
+    getOutputs();
+  }, []);
+
+  const getInputs = async () => {
+    const showInputs = await __GetInputs();
+    setInput(showInputs);
+    // console.log(showInputs);
+  };
+
+  const getOutputs = async () => {
+    const showOutputs = await __GetOutputs();
+    setOutput(showOutputs);
+    // console.log(showOutputs);
+  };
+
   return (
     <div className="saved-page">
-
       <div className="nav">
         {<Link to="/">home</Link>}
         {<Link to="/translate">translate</Link>}
       </div>
 
-      
+      <div className="inputs">
+        {input.map((inpt) => (
+          <div className="input-data" key={inpt.id}>
+            <h4>{inpt.text}</h4>
+          </div>
+        ))}
+      </div>
+
+      <div className="outputs">
+        {output.map((outpt) => (
+          <div className="output-data" key={outpt.id}>
+            <h4>{outpt.text}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
