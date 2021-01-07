82c5672bf8abcecb1c79ff638f07c4996eb10a0a
Jady Hom <hyjady@gmail.com>
2021-01-07 16:26:24 -0500

    saved.js -- organized code and fixed css

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { __GetInputs, __DeleteInputs } from "../services/InputService";
 import { __GetOutputs, __DeleteOutputs } from "../services/OutputService";
+import "../styles/Saved.css";
 
 const Saved = () => {
   const [input, setInput] = useState([]);
@@ -26,45 +27,76 @@ const Saved = () => {
 
   const handleDeleteInput = async (id) => {
     try {
-      await __DeleteInputs(id)
-      await getInputs()
+      await __DeleteInputs(id);
+      await getInputs();
     } catch (error) {
-      throw error
+      throw error;
     }
-  }
+  };
 
   const handleDeleteOutput = async (id) => {
     try {
-      await __DeleteOutputs(id)
-      await getOutputs()
+      await __DeleteOutputs(id);
+      await getOutputs();
     } catch (error) {
-      throw error
+      throw error;
     }
-  }
+  };
 
   return (
     <div className="saved-page">
+      <div className="top-left">
+        <div className="ex">x</div>
+        <div className="line">_</div>
+        <div className="oh">o</div>
+      </div>
+
       <div className="nav">
         {<Link to="/">home</Link>}
         {<Link to="/translate">translate</Link>}
       </div>
 
-      <div className="inputs">
-        {input.map((inpt) => (
-          <div className="input-data" key={inpt.id}>
-            <h4>{inpt.text}</h4>
-            <button onClick={() => handleDeleteInput(inpt.id)}>Delete</button>
+      <div className="inout-container">
+        <div className="inputs-container">
+          <div className="bartwo">
+            <h1>input</h1>
+            <li>–</li>
+            <li>x</li>
           </div>
-        ))}
-      </div>
-
-      <div className="outputs">
-        {output.map((outpt) => (
-          <div className="output-data" key={outpt.id}>
-            <h4>{outpt.text}</h4>
-            <button onClick={() => handleDeleteOutput(outpt.id)}>Delete</button>
+          <div className="inputs">
+            {input.map((inpt) => (
+              <div className="input-data" key={inpt.id}>
+                <h4>{inpt.text}</h4>
+                <button
+                  className="delete"
+                  onClick={() => handleDeleteInput(inpt.id)}
+                >
+                  🅧
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+        <div className="outputs-container">
+          <div className="bartwo">
+            <h1>output</h1>
+            <li>–</li>
+            <li>x</li>
+          </div>
+          <div className="outputs">
+            {output.map((outpt) => (
+              <div className="output-data" key={outpt.id}>
+                <h4>{outpt.text}</h4>
+                <button
+                  className="delete"
+                  onClick={() => handleDeleteOutput(outpt.id)}
+                >
+                  🅧
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
