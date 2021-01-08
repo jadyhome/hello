5f8c3d30a3bb1e8823106f0fae1bf4e20b6ccd55
Jady Hom <hyjady@gmail.com>
2021-01-08 11:37:20 -0500

    home.js -- css styled

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div className="home-page">
-      <div className="title">mother-tongue</div>
+      <div className="box-container">
+        <div className="home-bar">
+          <h1>mother-tongue</h1>
+          <li>–</li>
+          <li>x</li>
+        </div>
+
+        <div className="home-box">
+          <div className="textbox">
+            <p>
+              Hallo Mirëdita ታዲያስ مرحبا নমস্কার Zdravo Здравей Bok Ahoj Hej
+              Hallo Hello Saluton Tere Bula Terve Bonjour Hallo Γειά σου Aloha
+              שלום नमस्ते Sziasztok Halo Dia dhuit Ciao こんにちは 안녕하세요 안녕
+              ສະບາຍດີ Salve Sveiki Hallau Selamat tengahari Ħelow 你好 Kia ora
+              байна үү ဟယ်လို Cześć Hei Olá Alo Здравствуйте Salut Hola Hujambo
+              வணக்கம் สวัสดีคะ สวัสดีครับ Merhaba Salam Salom xin chào Mholweni
+              Sawubona 👋
+            </p>
+          </div>
+        </div>
+      </div>
+
       <div className="to-translate-page">
         {
           <Link to="/translate" className="click">
-            Click Me
+            click me
           </Link>
         }
       </div>
