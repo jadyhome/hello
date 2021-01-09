a43755a333f6fce38328bd3bde927383424fd943
Jady Hom <hyjady@gmail.com>
2021-01-08 20:34:01 -0500

    added languagerouter to approuter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,9 +2,11 @@ const Router = require("express").Router();
 const TranslateRouter = require("../routes/TranslateRouter");
 const InputRouter = require("../routes/InputRouter");
 const OutputRouter = require("../routes/OutputRouter");
+const LanguageRouter = require("../routes/LanguageRouter");
 
 Router.use("/translate", TranslateRouter);
 Router.use("/input", InputRouter);
 Router.use("/output", OutputRouter);
+Router.use("/language", LanguageRouter);
 
 module.exports = Router;
