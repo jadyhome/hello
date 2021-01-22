7ad784ad7cb6c189b23a4c11877ebc33b2b91012
Jady Hom <hyjady@gmail.com>
2021-01-22 14:42:48 -0500

    fixed post route

--- routes/CreateRouter.js
+++ routes/CreateRouter.js
@@ -1,7 +1,7 @@
 const Router = require("express").Router();
 const controller = require("../controllers/CreateController");
 
-Router.post("/create", controller.CreateTranslation);
+Router.post("/newtranslation", controller.CreateTranslation);
 Router.get("/createtranslations", controller.GetAllCreate);
 
 module.exports = Router;
