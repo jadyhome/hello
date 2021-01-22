cb9749a39a2beacd491f4957e0c53616efc36fc7
Jady Hom <hyjady@gmail.com>
2021-01-22 14:42:33 -0500

    added createrouter to approuter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -3,10 +3,12 @@ const TranslateRouter = require("../routes/TranslateRouter");
 const InputRouter = require("../routes/InputRouter");
 const OutputRouter = require("../routes/OutputRouter");
 const LanguageRouter = require("../routes/LanguageRouter");
+const CreateRouter = require("../routes/CreateRouter");
 
 Router.use("/translate", TranslateRouter);
 Router.use("/input", InputRouter);
 Router.use("/output", OutputRouter);
 Router.use("/language", LanguageRouter);
+Router.use("/create", CreateRouter);
 
 module.exports = Router;
