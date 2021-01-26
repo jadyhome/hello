1281e113312055ec4a320993c8cece103e41a8bd
Jady Hom <hyjady@gmail.com>
2021-01-26 16:24:45 -0500

    added commentrouter to approuter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -4,11 +4,13 @@ const InputRouter = require("../routes/InputRouter");
 const OutputRouter = require("../routes/OutputRouter");
 const LanguageRouter = require("../routes/LanguageRouter");
 const CreateRouter = require("../routes/CreateRouter");
+const CommentRouter = require("../routes/CommentRouter");
 
 Router.use("/translate", TranslateRouter);
 Router.use("/input", InputRouter);
 Router.use("/output", OutputRouter);
 Router.use("/language", LanguageRouter);
 Router.use("/create", CreateRouter);
+Router.use("/comment", CommentRouter);
 
 module.exports = Router;
